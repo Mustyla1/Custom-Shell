01899be98c1d70915a3ef2f4e9209e00d5c00a20
Oluwatobiloba-O <bamidurooluwatobiloba@gmail.com>
2023-12-15 20:27:23 -0800

    10

--- custom_shell_cd.c
+++ custom_shell_cd.c
@@ -1,10 +1,4 @@
 #include "custom_shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
-#define MAX_PATH_LENGTH 1024
 
 /**
  * custom_shell_cd - changes the working directory
@@ -13,61 +7,25 @@
  * @arguments: target directory
  * Return: 1 on success, 0 otherwise
  */
+
 int custom_shell_cd(char **arguments)
+
 {
-char *target_directory;
-char current_directory[MAX_PATH_LENGTH];
 
 if (arguments[1] == NULL)
-{
 
-target_directory = getenv("HOME");
-if (target_directory == NULL)
 {
-fprintf(stderr, "cd: HOME not set\n");
-return (0);
-}
-}
-else if (strcmp(arguments[1], "-") == 0)
-{
+fprintf(stderr, "expected argument to \"cd\"\n");
 
-target_directory = getenv("OLDPWD");
-if (target_directory == NULL)
-{
-fprintf(stderr, "cd: OLDPWD not set\n");
-return (0);
 }
-printf("%s\n", target_directory);
-}
+
 else
 {
-target_directory = arguments[1];
-}
-
-if (getcwd(current_directory, sizeof(current_directory)) == NULL)
+if (chdir(arguments[1]) != 0)
 {
-perror("getcwd");
-return (0);
+perror("error in custom_shell_cd.c: changing directory\n");
 }
-
-if (setenv("OLDPWD", current_directory, 1) != 0)
-{
-perror("setenv");
-return (0);
 }
-
-if (chdir(target_directory) != 0)
-{
-perror("chdir");
-return (0);
-}
-
-if (setenv("PWD", target_directory, 1) != 0)
-{
-perror("setenv");
-return (0);
-}
-
-return (1);
+return (-1);
 }
 
